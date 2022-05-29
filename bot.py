cf17857a61830f634542e8d3c6063ef348e89908
Henry Nelson <hcnelson99@gmail.com>
2022-05-29 19:58:00 +0100

    features

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 import secret
 from pprint import pprint
 import random
+import pickle
+import os
+from collections import defaultdict
 
 client = discord.Client()
 
@@ -35,14 +38,16 @@ def parse_die_roll(s):
         return None
     m = int(m)
     if neg: m = - m
+    c = int(c)
+    d = int(d)
+    if c < 1 or c > 100:
+        return None
+    if d < 1 or d > 1000:
+        return None
     return (int(c), int(d), m)
 
 def do_die_roll(die_roll):
         c, d, m = die_roll
-        if c < 1 or c > 100:
-            return
-        if d < 1 or d > 1000:
-            return
         rolls = []
         total = 0
         for _ in range(c):
@@ -63,6 +68,45 @@ def do_die_roll(die_roll):
         else:
             return str(total)
 
+def chompf(s, f):
+    i = s.find(f)
+    if i == -1:
+        return None
+    return s[:i], s[i+len(f):]
+
+class State:
+    def __init__(self):
+        if os.path.exists('storage'):
+            self.load()
+        else:
+            self.vars = defaultdict(dict)
+
+    def load(self):
+        with open('storage', 'rb') as f:
+            self.vars = pickle.load(f)
+
+    def save(self):
+        with open('storage', 'wb') as f:
+            pickle.dump(self.vars, f)
+
+    def set(self, author, var, n):
+        self.vars[author][var] = n
+        self.save()
+
+    def get(self, author, var):
+        r = self.vars[author][var]
+        if r.isdigit():
+            return int(r)
+        return do_die_roll(parse_die_roll(r))
+
+    def has(self, author, var):
+        return var in self.vars[author]
+
+    def backup(self, author):
+        l = [ f"set {v} {n}" for v, n in self.vars[author].items() ]
+        return '\n'.join(l)
+
+state = State()
 
 @client.event
 async def on_message(message):
@@ -73,10 +117,29 @@ async def on_message(message):
         return
 
     m = message.content
+    a = message.author.name
     die_roll = parse_die_roll(m)
     if die_roll:
         s = do_die_roll(die_roll)
         await message.reply(s)
+    elif m.startswith('set'):
+        _, m = chompf(m, 'set ')
+        v, n = chompf(m, ' ')
+        if not v.isalpha():
+            return
+        if n.isdigit():
+            state.set(a, v, int(n))
+            await message.reply(f'set {v} to {n}')
+        die_roll = parse_die_roll(n)
+        if die_roll:
+            state.set(a, v, n)
+            await message.reply(f'set {v} to {n}')
+    elif state.has(a, m):
+        s = state.get(a, m)
+        await message.reply(s)
+    elif m == 'backup':
+        await message.reply(state.backup(a))
 
 
-client.run(secret.TOKEN)
+if __name__ == '__main__':
+    client.run(secret.TOKEN)
